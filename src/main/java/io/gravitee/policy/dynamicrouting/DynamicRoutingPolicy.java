c1168903867956f0cba7be2f798d3fad2fc36623
brasseld <brasseld@gmail.com>
2016-06-21 16:55:18 +0200

    fix(dynamicrouting): Disable routing is no rule defined

--- src/main/java/io/gravitee/policy/dynamicrouting/DynamicRoutingPolicy.java
+++ src/main/java/io/gravitee/policy/dynamicrouting/DynamicRoutingPolicy.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,48 +57,53 @@ public class DynamicRoutingPolicy {
 
     @OnRequest
     public void onRequest(Request request, Response response, ExecutionContext executionContext, PolicyChain policyChain) {
-        String path = (String) executionContext.getAttribute(ExecutionContext.ATTR_RESOLVED_PATH);
-        LOGGER.debug("Dynamic routing for resolved path {} [complete path: {}]", path, request.path());
+        String path = request.path();
+        LOGGER.debug("Dynamic routing for path {}", request.path());
 
-        // Look for a matching pattern from rules
-        Optional<Rule> optRule = configuration.getRules().stream().filter(
-                rule -> rule.getPattern().matcher(path).matches()).findFirst();
+        if (configuration.getRules() != null && !configuration.getRules().isEmpty()) {
+            // Look for a matching pattern from rules
+            Optional<Rule> optRule = configuration.getRules().stream().filter(
+                    rule -> rule.getPattern().matcher(path).matches()).findFirst();
 
-        if (optRule.isPresent()) {
-            Rule rule = optRule.get();
+            if (optRule.isPresent()) {
+                Rule rule = optRule.get();
 
-            LOGGER.debug("Applying rule for path {}: [{} - {}]", path, rule.getPattern(), rule.getUrl());
-            String endpoint = rule.getUrl();
+                LOGGER.debug("Applying rule for path {}: [{} - {}]", path, rule.getPattern(), rule.getUrl());
+                String endpoint = rule.getUrl();
 
-            // Apply regex capture / replacement
-            Matcher match = rule.getPattern().matcher(path);
+                // Apply regex capture / replacement
+                Matcher match = rule.getPattern().matcher(path);
 
-            // Required to calculate capture groups
-            match.matches();
+                // Required to calculate capture groups
+                match.matches();
 
-            for(int idx = 0 ; idx < match.groupCount() ; idx++) {
-                String name = "group_" + (idx);
-                String value = match.group(idx+1);
+                for (int idx = 0; idx < match.groupCount(); idx++) {
+                    String name = "group_" + (idx);
+                    String value = match.group(idx + 1);
 
-                LOGGER.debug("Group count: {}", match.groupCount());
-                LOGGER.debug("Add a new variable in template engine: {} - {}", name, value);
-                executionContext.getTemplateEngine().getTemplateContext().setVariable(name, value);
+                    LOGGER.debug("Group count: {}", match.groupCount());
+                    LOGGER.debug("Add a new variable in template engine: {} - {}", name, value);
+                    executionContext.getTemplateEngine().getTemplateContext().setVariable(name, value);
+                }
+
+                // Given endpoint can be defined as the template using EL
+                LOGGER.debug("Transform endpoint {} using template engine", endpoint);
+                endpoint = executionContext.getTemplateEngine().convert(endpoint);
+
+                // Set final endpoint
+                executionContext.setAttribute(ExecutionContext.ATTR_REQUEST_ENDPOINT, endpoint);
+                LOGGER.debug("Route request to {}", endpoint);
+
+                // And continue request processing....
+                policyChain.doNext(request, response);
+            } else {
+                LOGGER.warn("No defined rule is matching path {}", path);
+                // No rule is matching request path
+                policyChain.failWith(PolicyResult.failure(HttpStatusCode.BAD_REQUEST_400, "No defined rule is matching path"));
             }
-
-            // Given endpoint can be defined as the template using EL
-            LOGGER.debug("Transform endpoint {} using template engine", endpoint);
-            endpoint = executionContext.getTemplateEngine().convert(endpoint);
-
-            // Set final endpoint
-            executionContext.setAttribute(ExecutionContext.ATTR_REQUEST_ENDPOINT, endpoint);
-            LOGGER.debug("Route request to {}", endpoint);
-
-            // And continue request processing....
-            policyChain.doNext(request, response);
         } else {
-            LOGGER.warn("No defined rule is matching path {}", path);
-            // No rule is matching request path
-            policyChain.failWith(PolicyResult.failure(HttpStatusCode.BAD_REQUEST_400, "No defined rule is matching path " + path));
+            // No rule defined
+            policyChain.doNext(request, response);
         }
     }
 }
